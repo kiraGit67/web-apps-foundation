0f0c1cc63e9b4ef22d49614b453a1325cb874556
kiraGit67 <kirsten@sais.de>
2023-02-20 12:46:46 +0100

    adds functional script to script.js

---
+++ 05-color-mixer/script.js
@@ -0,0 +1,45 @@
+const body = document.querySelector("body");
+
+const colorDisplay = document.querySelector(".color-display");
+
+const sliderRed = document.querySelector("input[type='range'].red");
+console.log(sliderRed.value);
+
+const sliderGreen = document.querySelector("input[type='range'].green");
+console.log(sliderGreen.value);
+
+const sliderBlue = document.querySelector("input[type='range'].blue");
+console.log(sliderBlue.value);
+
+colorDisplay.innerText =
+  "rgb(" +
+  sliderRed.value +
+  ", " +
+  sliderGreen.value +
+  ", " +
+  sliderBlue.value +
+  ")";
+console.log(colorDisplay.innerText);
+
+body.style.backgroundColor =
+  "rgb(" +
+  sliderRed.value +
+  ", " +
+  sliderGreen.value +
+  ", " +
+  sliderBlue.value +
+  ")";
+
+console.log(body.style.backgroundColor);
+
+function changed(slider) {
+  if (slider === sliderRed) {
+    sliderRed.value = slider.value;
+  }
+  if (slider === sliderGreen) {
+    sliderGreen.value = slider.value;
+  }
+  if (slider === sliderBlue) {
+    sliderBlue.value = slider.value;
+  }
+}
