4edf527cd27bce36847f9753cff99cd79ac00352
kiraGit67 <kirsten@sais.de>
2023-02-22 12:47:06 +0100

    fix: puts code into a function which works as an Event Listener

--- 05-color-mixer/script.js
+++ 05-color-mixer/script.js
@@ -5,41 +5,46 @@ const colorDisplay = document.querySelector(".color-display");
 const sliders = document.querySelectorAll("input[type='range']");
 console.log(sliders);
 
-const sliderRed = document.querySelector("input[type='range'].red");
+const sliderRed = document.querySelector("input[name='red']");
 console.log(sliderRed.value);
 
-const sliderGreen = document.querySelector("input[type='range'].green");
+const sliderGreen = document.querySelector("input[name='green']");
 console.log(sliderGreen.value);
 
-const sliderBlue = document.querySelector("input[type='range'].blue");
+const sliderBlue = document.querySelector("input[name='blue']");
 console.log(sliderBlue.value);
 
-const colorRGBvalue =
-  "rgb(" +
-  sliderRed.value +
-  ", " +
-  sliderGreen.value +
-  ", " +
-  sliderBlue.value +
-  ")";
+function updateColorValue() {
+  const colorRGBvalue =
+    "rgb(" +
+    sliderRed.value +
+    ", " +
+    sliderGreen.value +
+    ", " +
+    sliderBlue.value +
+    ")";
 
-// Turn step by step into hex value
-const rgbString = colorRGBvalue.split("(")[1].split(")")[0];
-const rgbArray = rgbString.split(",");
+  // Turn step by step into hex value
+  const rgbString = colorRGBvalue.split("(")[1].split(")")[0];
+  const rgbArray = rgbString.split(",");
 
-let hexArray = [];
+  let hexArray = [];
 
-for (let rgbItem of rgbArray) {
-  let hexItem = parseInt(rgbItem).toString(16);
-  if (hexItem.length === 1) {
-    hexArray.push("0" + hexItem);
-  } else {
-    hexArray.push(hexItem);
+  for (let rgbItem of rgbArray) {
+    let hexItem = parseInt(rgbItem).toString(16);
+    if (hexItem.length === 1) {
+      hexArray.push("0" + hexItem);
+    } else {
+      hexArray.push(hexItem);
+    }
   }
+
+  let hexString = "#" + hexArray.join("");
+
+  colorDisplay.innerText = hexString;
+  body.style.backgroundColor = hexString;
 }
 
-let hexString = "#" + hexArray.join("");
-
-colorDisplay.innerText = hexString;
-
-body.style.backgroundColor = hexString;
+sliderRed.addEventListener("input", updateColorValue);
+sliderGreen.addEventListener("input", updateColorValue);
+sliderBlue.addEventListener("input", updateColorValue);
