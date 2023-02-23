93faf8449db94a8cdbb7235d47afc3f601ef28d4
kiraGit67 <kirsten@sais.de>
2023-02-23 16:17:24 +0100

    feat: consolidates rgb2hex function and changeColorValues function; fix: adds "use strict"

--- 05-color-mixer/script.js
+++ 05-color-mixer/script.js
@@ -1,34 +1,28 @@
-const body = document.querySelector("body");
+"use strict";
 
+// Variables for Body- colorDisplay-
+const body = document.querySelector("body");
 const colorDisplay = document.querySelector(".color-display");
 
+// and slider input elements
 const sliders = document.querySelectorAll("input[type='range']");
-console.log(sliders);
-
 const sliderRed = document.querySelector("input[name='red']");
-console.log(sliderRed.value);
-
 const sliderGreen = document.querySelector("input[name='green']");
-console.log(sliderGreen.value);
-
 const sliderBlue = document.querySelector("input[name='blue']");
-console.log(sliderBlue.value);
 
+// Function to turn rgbValues to hexValues
 function rgb2hex(array) {
   let hexArray = [];
 
   for (let item of array) {
-    let hexItem = parseInt(item).toString(16);
-    if (hexItem.length === 1) {
-      hexArray.push("0" + hexItem);
-    } else {
-      hexArray.push(hexItem);
-    }
+    let hexItem = parseInt(item).toString(16).padStart(2, "0");
+    hexArray.push(hexItem);
   }
   let hexString = "#" + hexArray.join("");
   return hexString;
 }
 
+// Function to change the slider inputs' color values
 function updateColorValue() {
   const rgbArray = [sliderRed.value, sliderGreen.value, sliderBlue.value];
 
@@ -36,6 +30,6 @@ function updateColorValue() {
   body.style.backgroundColor = rgb2hex(rgbArray);
 }
 
-sliderRed.addEventListener("input", updateColorValue);
-sliderGreen.addEventListener("input", updateColorValue);
-sliderBlue.addEventListener("input", updateColorValue);
+sliders.forEach((slider) => {
+  slider.addEventListener("input", updateColorValue);
+});
